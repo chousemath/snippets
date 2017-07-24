91b76ef35e7b4a37128b72f8c261047a64613be8
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-24 22:32:25 +0900

    added more to the fmt practice script

--- fmt/main.go
+++ fmt/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	a := 123.321
+
 	fmt.Println("Hello world")
+	// width sets the minimum width of the field
+	// precision sets the number of places after the decimal place if appropriate
+	fmt.Printf("value of `a` with width=def, precision=def is %f\n", a)
+	fmt.Printf("value of `a` with width=def, precision=3 is %.3f\n", a)
+	fmt.Printf("value of `a` with width=def, precision=2 is %.2f\n", a)
+	fmt.Printf("value of `a` with width=def, precision=1 is %.1f\n", a)
+	fmt.Printf("type of `a` is %T\n", a)
 }
