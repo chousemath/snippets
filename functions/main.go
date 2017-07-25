e7de3c7629c85abd94b0caeec395bedef524fc21
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-25 11:12:25 +0900

    multiple returns from functions

--- functions/main.go
+++ functions/main.go
@@ -9,11 +9,25 @@ func main() {
 	divB := 25
 	modA := 11
 	modB := 10
+	a := 123.333
+	b := 111.111
+	aa := 13
+	bb := 10
 	fmt.Println("5 - 3 =", subtractInt(5, 3))
 	fmt.Printf("%d * %d = %d\n", multiplyA, multiplyB, multiplyInt(multiplyA, multiplyB))
 	fmt.Printf("%d / %d = %d\n", divA, divB, divideInt(divA, divB))
 	fmt.Printf("%d `mod` %d is %d\n", modA, modB, moduloInt(modA, modB))
 	fmt.Println("1 + 2 + 3 =", tripleSum(1, 2, 3))
+	sumAb, subAb := sumAndSubtractFloat(a, b)
+	fmt.Printf(
+		"sum of %g and %g is %.3f, sub of %g and %g is %.3f\n",
+		a, b, sumAb, a, b, subAb,
+	)
+	multAb, divAb, modAb := multAndDivideAndModInt(aa, bb)
+	fmt.Printf(
+		"%d * %d = %d, %d / %d = %d, %d `mod` %d = %d\n",
+		aa, bb, multAb, aa, bb, divAb, aa, bb, modAb,
+	)
 }
 
 func subtractInt(a, b int) int {
@@ -34,4 +48,12 @@ func moduloInt(a, b int) int {
 
 func tripleSum(a, b, c int) int {
 	return a + b + c
+}
+
+func sumAndSubtractFloat(a, b float64) (float64, float64) {
+	return a + b, a - b
+}
+
+func multAndDivideAndModInt(a, b int) (int, int, int) {
+	return a * b, a / b, a % b
 }
