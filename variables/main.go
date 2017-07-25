eb1301492d56cc20a7f2c37a74abb00bffebe0a3
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-25 11:37:04 +0900

    started adding some variables

--- variables/main.go
+++ variables/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 var a, b, c int
 var e, f, g bool
+var h, i, j float64 = 123.111, 234.222, 345.333
 
 func main() {
 	fmt.Printf("Zero values of a, b, and c are %d, %d, and %d\n", a, b, c)
@@ -16,4 +17,5 @@ func main() {
 	f = true
 	g = true || false
 	fmt.Printf("After assignment, they are %t, %t, and %t\n", e, f, g)
+	fmt.Printf("h = %g\ni = %g\nj = %g\n", h, i, j)
 }
