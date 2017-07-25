43e2d639234133b2f30bbfd52ba5818373c8c778
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-26 07:17:17 +0900

    bringing everything up to date

--- variables/main.go
+++ variables/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // should just use int unless there is a specific reason to use something different
 var a, b, c int
@@ -9,12 +12,15 @@ var h, i, j float64 = 123.111, 234.222, 345.333
 var aa, bb string
 
 func main() {
+	const PI = math.Pi
+
 	var k, l, m = true, false, "aloha"
 	n := "var 1"
 	o := "var 2"
 	p := "var 3"
 	q := 10
 	r := 3
+	s := 0.1 + 0.5i
 
 	fmt.Printf("Zero values of a, b, and c are %d, %d, and %d\n", a, b, c)
 	a = 1
@@ -32,6 +38,8 @@ func main() {
 
 	fmt.Printf("\nThe string zero values are\naa = %s, bb = %s\n", aa, bb)
 	fmt.Printf("q/r = %d / %d = %.3f\n", q, r, divFloat(float64(q), float64(r)))
+	fmt.Printf("Example of a complex number: %v of type %T\n", s, s)
+	fmt.Println("The value of pi is", PI)
 }
 
 func divFloat(a, b float64) float64 {
