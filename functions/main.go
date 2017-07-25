c02cf0b87d724d56cddbb32e80059db87906902a
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-25 11:03:18 +0900

    added a functions section

--- functions/main.go
+++ functions/main.go
@@ -7,9 +7,12 @@ func main() {
 	multiplyB := 6
 	divA := 100
 	divB := 25
+	modA := 11
+	modB := 10
 	fmt.Println("5 - 3 =", subtractInt(5, 3))
 	fmt.Printf("%d * %d = %d\n", multiplyA, multiplyB, multiplyInt(multiplyA, multiplyB))
 	fmt.Printf("%d / %d = %d\n", divA, divB, divideInt(divA, divB))
+	fmt.Printf("%d `mod` %d is %d\n", modA, modB, moduloInt(modA, modB))
 }
 
 func subtractInt(a, b int) int {
@@ -22,4 +25,8 @@ func multiplyInt(a, b int) int {
 
 func divideInt(a, b int) int {
 	return a / b
+}
+
+func moduloInt(a, b int) int {
+	return a % b
 }
